feeff266ecbdc4dd60fd09d422a450159864d2a1
Xiao <xiao@scp-079.org>
2020-08-03 10:56:02 +0000

    避免隐私泄漏

--- plugins/handlers/command.py
+++ plugins/handlers/command.py
@@ -101,13 +101,18 @@ def id_private(client: Client, message: Message) -> bool:
             text = get_info_user(message.from_user)
             return thread(send_message, (client, cid, text, mid))
 
+        # Avoid privacy issue
+        if username[0] in "-0123456789":
+            return command_error(client, message, lang("action_id"), lang("command_para"),
+                                 report=False, private=True)
+
         # Get the id
         the_type, the_id = resolve_username(client, username)
 
         # Check the id
         if not the_type or the_type not in {"channel", "user"} or not the_id:
-            return command_error(client, message, lang("action_id"), lang("command_para")
-                                 , report=False, private=True)
+            return command_error(client, message, lang("action_id"), lang("command_para"),
+                                 report=False, private=True)
 
         # User
         if the_type == "user":
