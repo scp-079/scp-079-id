7e99dd92b2a00f3188a789c80df65f06aa9fe79d
Xiao <xiao@scp-079.org>
2020-08-26 00:47:39 +0000

    微小改动

--- plugins/checker.py
+++ plugins/checker.py
@@ -118,5 +118,5 @@ def check_mode(values: Dict[str, Union[bool, bytes, int, str]], broken: bool) ->
 
 def raise_error(error: str):
     error = "-" * 24 + f"\nBot refused to start because:\n" + "-" * 24 + f"\n{error}" + "-" * 24
-    logger.critical(error)
+    logger.critical("\n" + error)
     raise SystemExit(error)
